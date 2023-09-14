975e5a5518eb2c25b21a4188fa2a40bcc90a5039
Willian <joiltonwillian@gmail.com>
2023-09-14 19:35:19 -0300

    alterações

--- js/main.js
+++ js/main.js
@@ -2,6 +2,29 @@
 const header = document.getElementById("test");
 
 
+const but1 = document.getElementById("but-modal1");
+const modal1 = document.getElementById("modal1");
+
+
+const but2 = document.getElementById("but-modal2");
+const modal2 = document.getElementById("modal2");
+
+
+const but3 = document.getElementById("but-modal3");
+const modal3 = document.getElementById("modal3");
+
+but1.addEventListener("click", () => clickModal(modal1))
+but2.addEventListener("click", () => clickModal(modal2))
+but3.addEventListener("click", () => clickModal(modal3))
+
+
+function clickModal(modal) {
+    modal.showModal();
+}
+
+
+
+
 function scrollando() {
     if(window.scrollY !==0){
         header.classList.add("hidden-active")
@@ -17,9 +40,4 @@ function scrollando() {
 
 document.addEventListener("scroll", (event) => scrollando());
 
-window.onload(scrollando())
-
-
-
-
-
+Window.onload = scrollando();
